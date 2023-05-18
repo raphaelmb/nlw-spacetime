60f6932234edb6d090df16c76e958ea53fa6fefa
Raphael Berger <raphaelmb@gmail.com>
2023-05-18 14:03:35 -0300

    frontend refactor + oauth setup

---
+++ frontend/src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Done with ❤️ at{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://rocketseat.com.br"
+      >
+        Rocketseat
+      </a>
+      &apos;s NLW
+    </div>
+  )
+}
---
+++ notes.md
@@ -0,0 +1,14 @@
+# Setup
+Github > Settings > Developer settings > OAuth Apps 
+
+## Flow
+Application name: nlw-spacetime (dev)
+
+Homepage URL: http://localhost:3000
+
+Authorization callback URL: http://localhost:3000/api/auth/callback
+
+### Info
+backend: Client ID and Secret
+
+frontend: Client ID
