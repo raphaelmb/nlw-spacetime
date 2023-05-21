52aafbabc9e3bf16f690deff315724328257836d
Raphael Berger <raphaelmb@gmail.com>
2023-05-21 19:18:51 -0300

    frontend add list layout + get memories from api

--- frontend/src/components/EmptyMemories.tsx
+++ frontend/src/components/EmptyMemories.tsx
@@ -1,6 +1,6 @@
 export function EmptyMemories() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
         You have not registered a memory yet,{' '}
         <a href="" className="underline hover:text-gray-50">
